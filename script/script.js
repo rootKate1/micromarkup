08eea4e046c3343b87c0fe3bfafb6e1e99382354
rootKate1 <ekaterinaulasheva@gmail.com>
2025-03-22 15:21:04 +0300

    Delete script directory

--- script/script.js
+++
@@ -1,7 +0,0 @@
-let Btn = document.getElementById('btn-link');
-Btn.addEventListener('click', function(e){
-    let Block = document.getElementById('link');
-    Block.classList.toggle('display-none');
-    Btn.classList.toggle('navbar-toggler-icon');
-    Btn.classList.toggle('btn-close')
-})
